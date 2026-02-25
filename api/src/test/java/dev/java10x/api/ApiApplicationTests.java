b079853ad450f88a3972d7d9b009fc0b111dcfda
Felipe Drehmer <felipeka.drehmer@gmail.com>
2026-02-25 14:15:58 -0300

    Inicializado projeto

---
+++ api/src/test/java/dev/java10x/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
